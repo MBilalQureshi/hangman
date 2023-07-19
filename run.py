63d5af4c33260be5611805ac733b07bd48687b58
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-07-19 17:48:59 +0000

    Move code for invalid answers from user in a function and comment code

--- run.py
+++ run.py
@@ -3,27 +3,34 @@ from datetime import datetime
 import time
 
 
-class game:
-    def __init__(self, game_name, user_name, no_of_stages, total_win, total_loose):
+class Game:
+    """
+    Purpose of this class is to manage the game data and push the data to the
+    spread sheet.
+    As parent game class, this will also handle multiple games in future
+    other than hangman.
+    """
+    def __init__(self, game_name, user_name, no_of_stages, total_wins, total_loses):
         self.game_name = game_name
         self.user_name = user_name
         self.no_of_stages = no_of_stages
-        self.total_win = total_win
-        self.total_loose = total_loose
+        self.total_wins = total_wins
+        self.total_loses = total_loses
 
     def details(self):
+        
         now = datetime.now()
-        return f"Game name is {self.game_name}, Game stages are {self.no_of_stages}, Username is {self.user_name}, data and time is:{now.strftime('%d/%m/%Y %H:%M:%S')}, total won{self.total_win}, total loose{self.total_loose}"
+        return f"Game name is {self.game_name}, Game stages are {self.no_of_stages}, Username is {self.user_name}, data and time is:{now.strftime('%d/%m/%Y %H:%M:%S')}, total won{self.total_wins}, total loose{self.total_loses}"
 
-class hangman(game):
+
+class Hangman(Game):
+    """
+    This class handles operations of hangman man, random selection of
+    words, asking which word is it, calculate false and correct
+    answers and handle multiple stages from 1 - 5 mentioned by user.
+    """
     words = ["apple", "banana", "owl", "controller", "helipad", "semiconductor"]
-
-    def __init__(self, user_name, no_of_stages):
-        game.__init__(self, "Hang Man", user_name, no_of_stages, 0, 0)
-
-
-    def show_secret_word(self, secret_word, stage_number):
-        hangman_pics = ['''
+    hangman_pics = ['''
   +---+
       |
       |
@@ -80,13 +87,56 @@ class hangman(game):
  / \  |
       |
 =========''']
+
+    def __init__(self, user_name, no_of_stages):
+        Game.__init__(self, "Hang Man", user_name, no_of_stages, 0, 0)
+
+    def calculate_invalid_answers(self, deaths, secret_word, stage_number):
+        """
+        This function calculates the invalid answers
+        and generate hangman images based on it.
+        It also tells on 7th try that user had failed and 
+        check if there is next stage or not
+        """
+        if deaths == 1:
+            print(Hangman.hangman_pics[1])
+        elif deaths == 2:
+            print(Hangman.hangman_pics[2])
+        elif deaths == 3:
+            print(Hangman.hangman_pics[3])
+        elif deaths == 4:
+            print(Hangman.hangman_pics[4])
+        elif deaths == 5:
+            print(Hangman.hangman_pics[5])
+        elif deaths == 6:
+            print(Hangman.hangman_pics[6])
+        else:
+            print(Hangman.hangman_pics[7])
+            self.total_loses += 1
+            print("The String was: "+secret_word.upper()+'\n')
+            print("Well, Hang Man is dead and it's on you :(\n")
+            time.sleep(1.5)
+            if self.no_of_stages != stage_number:
+                print("Loading next stage...\n")
+                time.sleep(1.5)
+                # break
+
+    def start_hangman(self, secret_word, stage_number):
+        """
+        The Hangman game start from here user give a character
+        the algorithm checks if character was already given, if not
+        it checks if it matches the one that was randomly generated,
+        if it does, user is closer to save hangman, if this is also
+        not the situation, this function puts hangman to near death by call
+        another function
+        """
         word_length = len(secret_word)
         update_chars = ''
 
         for x in range(word_length):
             update_chars += '_'
         deaths = 0
-        print(hangman_pics[0])
+        print(Hangman.hangman_pics[0])
         while deaths < 7:
             print("\nYour String: "+update_chars.upper()+'\n')
             char = input("Please Enter character\n").lower()
@@ -102,10 +152,10 @@ class hangman(game):
                         for set_char in position:
                             update_chars = update_chars[:set_char] \
                                 + char + update_chars[set_char+1:]
-                        # print(update_chars+'\n')
                         if update_chars == secret_word:
                             print(secret_word.upper()+'\n')
                             print("Congratulations, You have won this stage\n")
+                            self.total_wins += 1
                             if self.no_of_stages != stage_number:
                                 print("Loading next stage...\n")
                                 time.sleep(1.5)
@@ -113,38 +163,27 @@ class hangman(game):
                                 time.sleep(1.5)
                             break
                     else:
-                        # count deaths
                         deaths += 1
-                        if deaths == 1:
-                            print(hangman_pics[1])
-                        elif deaths == 2:
-                            print(hangman_pics[2])
-                        elif deaths == 3:
-                            print(hangman_pics[3])
-                        elif deaths == 4:
-                            print(hangman_pics[4])
-                        elif deaths == 5:
-                            print(hangman_pics[5])
-                        elif deaths == 6:
-                            print(hangman_pics[6])
-                        else:
-                            print(hangman_pics[7])
-                            print("The String was: "+secret_word.upper()+'\n')
-                            print("Well, Hang Man is dead and it's on you :(\n")
-                            time.sleep(1.5)
-                            if self.no_of_stages != stage_number:
-                                print("Loading next stage...\n")
-                                time.sleep(1.5)
-                                break
+                        Hangman.calculate_invalid_answers(self, deaths, secret_word, stage_number)
+                        
             else:
                 print("Kindly enter single alphabet character\n")
 
     def start_game(self):
+        """
+        This functions just fetch random word from words list and move it 
+        to another function along with total stages
+        """
         for stage_number in range(0, self.no_of_stages):
-            secret_word = hangman.words.pop(randrange(len(hangman.words)))
-            hangman.show_secret_word(self, secret_word, stage_number+1)
+            secret_word = Hangman.words.pop(randrange(len(Hangman.words)))
+            Hangman.start_hangman(self, secret_word, stage_number+1)
+
 
 def name_validation():
+    """
+    This function validates the username 
+    entered by the user
+    """
     while True:
         name = input("Please Enter Name\n")
         if name.replace(" ", "").isalpha():
@@ -154,6 +193,11 @@ def name_validation():
 
 
 def stages_count_validation():
+    """
+    This function sets the number of stages entered
+    by the user from 1 to 5 and validates the data
+    entered by the user.
+    """
     while True:
         stages = input("Pleas Enter number of stages between 1 and 5\n")
         try:
@@ -171,12 +215,28 @@ def stages_count_validation():
 
 
 def main():
+    """
+    This is the main function, The hangman logo is here, The
+    username is asked in the begining so that if user wants to
+    keep playing he/she dosen't have to enter username again and
+    again.
+    the user is asked if he/she want to keep playing or not once
+    they had played every stage.
+    """
+    print(" _                                             ")
+    print("| |                                            ")
+    print("| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  ")
+    print("| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ ")
+    print("| | | | (_| | | | | (_| | | | | | | (_| | | | |")
+    print("|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|")
+    print("                    __/ |                      ")
+    print("                   |___/                       ")
     name = name_validation()
     while True:
         set_game = input("\nDo you want to start the game? Press y for yes or any other button for no.\n").lower()
         if set_game == 'y':
             total_stages = stages_count_validation()
-            hang_man = hangman(name, total_stages)
+            hang_man = Hangman(name, total_stages)
             hang_man.start_game()
             x = hang_man.details()
             print(x)
@@ -185,14 +245,5 @@ def main():
             time.sleep(1.5)
             break
 
-
-print(" _                                             ")
-print("| |                                            ")
-print("| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  ")
-print("| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ ")
-print("| | | | (_| | | | | (_| | | | | | | (_| | | | |")
-print("|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|")
-print("                    __/ |                      ")
-print("                   |___/                       ")
 
 main()
