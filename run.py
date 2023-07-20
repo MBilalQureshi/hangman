1675a5cd2cb34de6eac869e8898f706aee361301
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-07-20 11:52:33 +0000

    Import gspread and google auth into the project

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
+import gspread
+from google.oauth2.service_account import Credentials
 from random import randrange
 from datetime import datetime
 import time
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Hangman')
 
 
 class Game:
